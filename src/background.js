8b7de278898853f9b43969c89c0ee12e457c0bf8
mugitea <mugitea7@gmail.com>
2020-05-27 12:27:03 +0900

    [add]'background.js' can search by selected word.

--- src/background.js
+++ src/background.js
@@ -1,14 +1,11 @@
 chrome.contextMenus.create({
     title: 'Search Tool',
+    contexts: ['selection'],
+    type: 'normal',
     onclick: (info, tab) => {
-        window.alert('Hello,ChromeExtension!');
+        var selection_text = info.selectionText;
+        var search_url = "https://www.google.com/search?q=" + encodeURI(selection_text);
+
+        window.open(search_url, "_blank");
     }
 });
-
-/*
-$(() => {
-    $("#save").click(() => {
-        localStorage["message"] = $("#message").val();
-    });
-});
-*/
