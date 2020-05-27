f78a6e7eb14e48d2cd2b58c9883ff1675b379262
mugitea <mugitea7@gmail.com>
2020-05-27 20:28:56 +0900

    [change] 'background.js' change tab count.

--- src/background.js
+++ src/background.js
@@ -1,11 +1,13 @@
-chrome.contextMenus.create({
-    title: 'Search Tool',
-    contexts: ['selection'],
-    type: 'normal',
-    onclick: (info, tab) => {
-        var selection_text = info.selectionText;
-        var search_url = "https://www.google.com/search?q=" + encodeURI(selection_text);
+for (var count = 0; count <= 5; count++) {
+    chrome.contextMenus.create({
+        title: "data" + count,
+        contexts: ['selection'],
+        type: 'normal',
+        onclick: (info, tab) => {
+            var selection_text = info.selectionText;
+            var search_url = "https://www.google.com/search?q=" + encodeURI(selection_text);
 
-        window.open(search_url, "_blank");
-    }
-});
+            window.open(search_url, "_blank");
+        }
+    });
+}
