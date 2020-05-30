f283e72aac2d978b963cac738cb90f6e7d7b9d5e
mugitea <mugitea7@gmail.com>
2020-05-30 19:09:10 +0900

    [bugFix] オプション初期値関連のバグを解消.

--- src/background.js
+++ src/background.js
@@ -1,49 +1,54 @@
-chrome.runtime.onStartup.addListener(function () {
-    const url = chrome.runtime.getURL('src/default.json');
-    fetch(url)
-        .then(res => res.json())
-        .then(json => {
-            localStorage.setItem('option', JSON.stringify(json));
-        });
-});
-
-const parent_menu = chrome.contextMenus.create({
-    id: "parent",
-    title: "SearchTool",
-    contexts: ['selection'],
-    type: "normal",
-});
-
-const replace_string = "{{}}";
-
-const load_data = JSON.parse(localStorage.getItem("option"));
-
-if (load_data !== undefined && load_data !== null) {
-
-    for (let index = 0; index < load_data.length; index++) {
-
-        let title_str = load_data[index].name;
-        if (title_str.match(/^[ 　\r\n\t]*$/))
-            title_str = "No title";
-
-        chrome.contextMenus.create({
-            title: title_str,
-            contexts: ['selection'],
-            type: 'normal',
-            parentId: parent_menu,
-            onclick: (info, tab) => {
-                const selection_text = info.selectionText;
-                const search_url = load_data[index].query.replace(replace_string, encodeURI(selection_text));
-
-                window.open(search_url, "_blank");
-            }
-        });
+chrome.runtime.onInstalled.addListener(function () {
+    if (!localStorage.getItem('is_setted')) {
+        const url = chrome.runtime.getURL('src/default.json');
+        fetch(url)
+            .then(res => res.json())
+            .then(json => {
+                localStorage.setItem('option', JSON.stringify(json));
+            });
     }
-} else {
-    chrome.contextMenus.create({
-        title: '検索が設定されていません.',
+    const parent_menu = chrome.contextMenus.create({
+        id: "parent",
+        title: "SearchTool",
         contexts: ['selection'],
-        type: 'normal',
-        parentId: parent_menu,
+        type: "normal",
     });
-}
+    
+    setTimeout(function () {
+        localStorage.setItem('is_setted', 'hoge');
+        const replace_string = "{{}}";
+        
+        const load_data = JSON.parse(localStorage.getItem("option"));
+        
+        if (load_data !== undefined && load_data !== null) {
+        
+            for (let index = 0; index < load_data.length; index++) {
+        
+                let title_str = load_data[index].name;
+                if (title_str.match(/^[ 　\r\n\t]*$/))
+                    title_str = "No title";
+        
+                chrome.contextMenus.create({
+                    title: title_str,
+                    contexts: ['selection'],
+                    type: 'normal',
+                    parentId: parent_menu,
+                    onclick: (info, tab) => {
+                        const selection_text = info.selectionText;
+                        const search_url = load_data[index].query.replace(replace_string, encodeURI(selection_text));
+        
+                        window.open(search_url, "_blank");
+                    }
+                });
+            }
+        } else {
+            chrome.contextMenus.create({
+                title: '検索が設定されていません.',
+                contexts: ['selection'],
+                type: 'normal',
+                parentId: parent_menu,
+            });
+        }
+    }, 100);
+
+});
